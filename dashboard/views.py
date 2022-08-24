8bbc0c5a662e311870a571238fc77e9aefcced6b
TZbooo <helloworldbooo@gmail.com>
2022-08-24 16:23:48 +0300

    fix static files path in dashboard.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard.html'
     login_url = '/#popup-login/'
-    redirect_field_name = 'redirect_to'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
