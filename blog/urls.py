010709fa7f2a619ae6fa953a55524bd738d19fd1
TZbooo <helloworldbooo@gmail.com>
2022-08-29 12:34:14 +0300

    replace TemplateView to ListView in BlogPageView class and to DetailView in AeticlePageView, after now i absolutely not override get_context_data method in blog application views

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogPageView.as_view(), name='blog'),
-    path('article/<int:article_id>', views.ArticlePageView.as_view()),
+    path('article/<int:article_pk>', views.ArticlePageView.as_view(), name='article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import ListView
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
 
 from .models import ArticleModel
 from utils import PopupCookiesContextMixin, PopupAuthContextMixin
@@ -8,13 +7,12 @@ from utils import PopupCookiesContextMixin, PopupAuthContextMixin
 class BlogPageView(PopupCookiesContextMixin, PopupAuthContextMixin, ListView):
     model = ArticleModel
     template_name = 'blog/blog.html'
+    context_object_name = 'articles_previews'
 
 
-class ArticlePageView(PopupCookiesContextMixin, PopupAuthContextMixin, TemplateView):
+class ArticlePageView(PopupCookiesContextMixin, PopupAuthContextMixin, DetailView):
+    model = ArticleModel
     template_name = 'blog/article-page.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['article'] = ArticleModel.objects.get(pk=kwargs.get('article_id'))
-        return context
+    pk_url_kwarg = 'article_pk'
+    context_object_name = 'article'
     
