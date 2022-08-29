1ab2a0f5c4511b85d1aa2e63c03f1251eeac4324
stanhjr <stanislav.o@cybearsoft.com>
2022-08-29 10:07:57 +0300

    fix arcticle

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class ArticleModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
-    content_text = models.CharField(max_length=100_000)
+    content_text = models.TextField(max_length=100_000)
     preview_text = models.CharField(max_length=130)
     content_image = models.ImageField(upload_to='content_images/')
     preview_image = models.ImageField(upload_to='preview_images/')
+
+    class Meta:
+        ordering = ('-created_at', )
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,20 @@
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
 from .models import ArticleModel
 from utils import PopupCookiesContextMixin, PopupAuthContextMixin
 
 
-class BlogPageView(PopupCookiesContextMixin, PopupAuthContextMixin, TemplateView):
+class BlogPageView(PopupCookiesContextMixin, PopupAuthContextMixin, ListView):
+    model = ArticleModel
     template_name = 'blog/blog.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        context['article_previews'] = ArticleModel.objects.all().order_by('-pk')
-
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #
+    #     context['article_previews'] = ArticleModel.objects.all().order_by('-pk')
+    #
+    #     return context
     
 
 class ArticlePageView(PopupCookiesContextMixin, PopupAuthContextMixin, TemplateView):
