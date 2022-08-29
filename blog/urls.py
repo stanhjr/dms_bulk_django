5fade15dd33e2ef10cd4c6d2278bb98bdb5fbfe7
TZbooo <helloworldbooo@gmail.com>
2022-08-29 18:55:13 +0300

    make blog page pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.BlogPageView.as_view(), name='blog'),
-    path('article/<int:article_pk>', views.ArticlePageView.as_view(), name='article'),
+    path('<int:page_pk>/', views.BlogPageView.as_view(), name='blog'),
+    path('article/<int:article_pk>/',
+         views.ArticlePageView.as_view(), name='article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,30 @@ from .models import ArticleModel
 from utils import PopupCookiesContextMixin, PopupAuthContextMixin
 
 
-class BlogPageView(PopupCookiesContextMixin, PopupAuthContextMixin, ListView):
+class BlogPageView(PopupCookiesContextMixin, PopupAuthContextMixin, DetailView):
     model = ArticleModel
     template_name = 'blog/blog.html'
-    context_object_name = 'articles_previews'
+    pk_url_kwarg = 'page_pk'
+    context_object_name = 'page_pk'
+
+    def get_context_data(self, **kwargs):
+        page_pk = self.kwargs.get('page_pk')
+
+        context = super().get_context_data(**kwargs)
+        articles = ArticleModel.objects.all().order_by(
+            '-created_at')[(page_pk - 1) * 6:]
+
+        if page_pk * 6 < len(articles):
+            articles = articles[:page_pk * 6]
+
+        elif 6 < len(articles):
+            articles = articles[:6]
+
+        context['articles_previews'] = articles
+        context['current_page_pk'] = page_pk
+        context['current_page_is_divided_into_3'] = True if not page_pk % 3 else False
+        context['current_page_pk_is_even'] = True if not page_pk % 2 else False
+        return context
 
 
 class ArticlePageView(PopupCookiesContextMixin, PopupAuthContextMixin, DetailView):
