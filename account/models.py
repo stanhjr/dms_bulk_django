d297eae0d10b79cf727f9f179bfd659b96b927a0
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-09-01 14:27:56 +0300

    add dms_token and cents fields in custom user model

--- account/models.py
+++ account/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     receive_news = models.BooleanField(default=True)
     receive_activity = models.BooleanField(default=True)
+
+    dms_tokens = models.BigIntegerField(default=0)
+    cents = models.BigIntegerField(default=0)
