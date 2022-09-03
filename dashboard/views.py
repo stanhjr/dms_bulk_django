965f5924f2decffb2c2e0fe64ac72124fbd43fd7
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-09-03 23:40:02 +0300

    add page var to context data

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
 from utils import PopupCookiesContextMixin
+from order import models
 
 
-class DashboardPageView(PopupCookiesContextMixin, LoginRequiredMixin, TemplateView):
+class DashboardPageView(PopupCookiesContextMixin, LoginRequiredMixin, ListView):
     template_name = 'dashboard/dashboard.html'
     login_url = reverse_lazy('home')
+    model = models.OrderModel
+    context_object_name = 'recent_orders'
+
+    def get_queryset(self):
+        return self.model.objects.filter(order_calc__user=self.request.user)[:3]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
