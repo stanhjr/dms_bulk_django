5216798ed1e76b970edb2efb10ada4aa7b1eecd0
stanhjr <stanislav.o@cybearsoft.com>
2022-09-10 17:25:34 +0300

    add docs celery tasks

--- order/utils.py
+++ order/utils.py
@@ -2,7 +2,9 @@ from django.contrib.auth.mixins import AccessMixin
 
 
 class ConfirmRequiredMixin(AccessMixin):
-    '''Verify that current user confirm his account by email'''
+    """
+    Verify that current user confirm his account by email
+    """
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_confirmed:
