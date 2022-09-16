289427984a1facaa4e6b0fcda099a4627c948c87
stanhjr <stanislav.o@cybearsoft.com>
2022-09-16 10:14:47 +0300

    change styles boardAdmin

--- order/admin.py
+++ order/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput, Textarea
 
-from . import models
+from order.models import BoardModel, OrderCalcModel, OrderModel
 
 
 class OrderModelAdmin(admin.ModelAdmin):
     exclude = ('sending_start_at', 'send_messages_speed', 'completed')
 
 
-admin.site.register(models.BoardModel)
-admin.site.register(models.OrderModel, OrderModelAdmin)
-admin.site.register(models.OrderCalcModel)
+class YourModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
+    }
+
+    class Media:
+        css = {"all": ("css/custom_admin.css",)}
+
+
+admin.site.register(BoardModel, YourModelAdmin)
+admin.site.register(OrderModel, OrderModelAdmin)
+admin.site.register(OrderCalcModel)
