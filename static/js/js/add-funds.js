f42bb4c0bdb2ccc214c0b93249ee8b44fdf38670
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-09-17 14:30:03 +0300

    create feature: display dynamic invoice modal window

--- static/js/js/add-funds.js
+++ static/js/js/add-funds.js
@@ -17,3 +17,20 @@ $('.btn.js-popup').click(() => {
     )
     $('#create_invoice_calc_form').submit()
 })
+
+$('.open-invoice-btn').click(event => {
+    const current_invoice_table = new Array()
+    $(event.target).closest('.invoice-table').children().each((i, element) => current_invoice_table.push($(element).text()))
+    
+    $('.table-info > .tr:eq(1) > .td:eq(0)').text(current_invoice_table[0])
+    $('.table-info > .tr:eq(1) > .td:eq(1)').text(current_invoice_table[1])
+    $('.table-info > .tr:eq(1) > .td:eq(2)').text(current_invoice_table[2])
+    $('.table-info > .tr:eq(1) > .td:eq(3) > span').text(current_invoice_table[4])
+
+    $('.info-wrap > div:eq(1) > p').text(current_invoice_table[5])
+
+    $('.table-total > .tr').each((i, element) => $(element).find('.td:eq(1)').text(current_invoice_table[3]))
+
+    $('.header-wrap > .right > p').text(`ID (#${current_invoice_table[0]})`)
+    console.log(current_invoice_table)
+})
