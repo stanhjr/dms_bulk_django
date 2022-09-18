c19d0285500bce165490852010ad067f62679c58
stanhjr <stanislav.o@cybearsoft.com>
2022-09-18 14:17:49 +0300

    fix admin

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
 
-from . import models
+from order.models import BoardModel
+from order.models import OrderCalcModel
+from order.models import OrderModel
+from order.models import Coupon
 
 
 class OrderModelAdmin(admin.ModelAdmin):
@@ -14,12 +19,21 @@ class OrderCalcModelAdmin(admin.ModelAdmin):
     list_display = ('social_network', 'amount', 'discount', 'total', 'created_at', 'user')
 
 
+class BoardModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
+    }
+
+    class Media:
+        css = {"all": ("css/custom_admin.css",)}
+
+
 class CouponAdmin(admin.ModelAdmin):
     search_fields = ['name', ]
     list_display = ('name', 'discount', 'left_to_use', 'uses')
 
 
-admin.site.register(models.BoardModel)
-admin.site.register(models.OrderModel, OrderModelAdmin)
-admin.site.register(models.OrderCalcModel, OrderCalcModelAdmin)
-admin.site.register(models.Coupon, CouponAdmin)
+admin.site.register(BoardModel, BoardModelAdmin)
+admin.site.register(OrderModel, OrderModelAdmin)
+admin.site.register(OrderCalcModel, OrderCalcModelAdmin)
+admin.site.register(Coupon, CouponAdmin)
