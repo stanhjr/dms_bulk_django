452ae704f4feb8441893eaadb30284d8262e65bb
stanhjr <stanislav.o@cybearsoft.com>
2022-10-10 13:01:16 +0300

    add balance update celery task

--- celery_tasks/__init__.py
+++ celery_tasks/__init__.py
@@ -2,3 +2,4 @@ from .tasks import generate_key
 from .tasks import send_verify_link_to_email
 from .tasks import send_reset_password_link_to_email
 from .tasks import send_welcome_to_dmsbulk_message_after_success_verifing
+from .tasks import send_balance_update
