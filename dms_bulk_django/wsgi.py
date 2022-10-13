61f4b550e40eefe5afd303b01e78712f44a9b0f8
stanhjr <stanislav.o@cybearsoft.com>
2022-10-13 20:21:17 +0300

    add wsgi

--- dms_bulk_django/wsgi.py
+++ dms_bulk_django/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dms_bulk_django.settings')
 
