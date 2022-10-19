dcc8b0431a3e431cc0e7fa9e149082282fb9f600
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-10-19 14:53:40 +0300

    fix seo

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:page_pk>/', views.BlogPageView.as_view(), name='blog'),
+    path('', views.BlogPageView.as_view(), name='blog'),
     path('article/<slug:article_slug>/', views.ArticlePageView.as_view(), name='article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.http import Http404
 
 from .models import ArticleModel
@@ -8,32 +8,14 @@ from utils import PopupAuthContextMixin
 from utils import MetaInfoContextMixin
 
 
-class BlogPageView(MetaInfoContextMixin, PopupCookiesContextMixin, PopupAuthContextMixin, DetailView):
-    model = ArticleModel
+class BlogPageView(MetaInfoContextMixin, PopupCookiesContextMixin, PopupAuthContextMixin, ListView):
     template_name = 'blog/blog.html'
-    pk_url_kwarg = 'page_pk'
-    context_object_name = 'page_pk'
-
-    def get_object(self, queryset=None):
-        try:
-            return super().get_object(queryset)
-        except Http404:
-            pass
+    model = ArticleModel
+    context_object_name = 'articles'
+    paginate_by = 6
 
     def get_context_data(self, **kwargs):
-        page_pk = self.kwargs.get('page_pk')
-
         context = super().get_context_data(**kwargs)
-        articles = ArticleModel.objects.all()[(page_pk - 1) * 6:]
-
-        if page_pk * 6 < len(articles):
-            articles = articles[:page_pk * 6]
-
-        elif 6 < len(articles):
-            articles = articles[:6]
-
-        context['articles_previews'] = articles
-        context['current_page_pk'] = page_pk
         context['page'] = 'blog'
         return context
 
