447d38386d79ccd8fa2c727672ed8d93b42344bf
stanhjr <stanislav.o@cybearsoft.com>
2022-10-19 16:09:01 +0300

    add google template

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from core import views
 
 urlpatterns = [
     path('robots.txt/', views.RobotsTxtPageView.as_view(), name='robots'),
+    path('google59f76306dc2eaed9.html/', views.GoogleTxtPageView.as_view(), name='robots'),
+
 ]
